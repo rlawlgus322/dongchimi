7285ea51ddcc5ef6981b0ae14a2e8387dfe02f16
김형준 <khj58777@naver.com>
2020-10-22 19:14:51 +0900

    [~ing] S03P31A409-12 jpa연결 확인

--- Backend/src/main/java/com/dcm/model/User.java
+++ Backend/src/main/java/com/dcm/model/User.java
@@ -3,12 +3,16 @@ package com.dcm.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.*;
 
 @Setter
 @Getter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "userinfo")
 public class User {
 
     @Id
