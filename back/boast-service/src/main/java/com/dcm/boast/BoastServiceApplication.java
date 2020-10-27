b66374121c2f2ab7b77af9150a11a25e22d81822
김지현 <rlawlgus322@naver.com>
2020-10-27 14:48:01 +0900

    [~ing] S03P31A409-61 : 자랑하기 생성

---
+++ back/boast-service/src/main/java/com/dcm/boast/BoastServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dcm.boast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoastServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoastServiceApplication.class, args);
+	}
+
+}
