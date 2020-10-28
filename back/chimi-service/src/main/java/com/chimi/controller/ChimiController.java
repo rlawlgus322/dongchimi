c42e44e6b399322c41864868a14558d11fd84ea6
고유진 <smile970511@gmail.com>
2020-10-28 17:57:40 +0900

    [feature] S03P31A409-66 S03P31A409-67  : 취미파티 모두 조회(페이징), 상세 조회

--- back/chimi-service/src/main/java/com/chimi/controller/ChimiController.java
+++ back/chimi-service/src/main/java/com/chimi/controller/ChimiController.java
@@ -1,10 +1,18 @@
 package com.chimi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,5 +52,23 @@ public class ChimiController {
 		}
 		if(newChimi != null)	return new ResponseEntity<>("success", HttpStatus.OK);
 		else					return new ResponseEntity<>("fail", HttpStatus.BAD_REQUEST);
+	}
+	
+	@GetMapping
+	@ApiOperation(value = "모든 취미 파티 조회")	// ?page=0&size=20&sort=hid,asc
+	public ResponseEntity<Page<Chimi>> searchAll(@PageableDefault(size=10, sort="createdate",direction = Sort.Direction.DESC)Pageable pageable){
+		
+		Page<Chimi> list = chimiService.findAll(pageable);
+	
+		if(list != null)		return new ResponseEntity<>(list, HttpStatus.OK);
+		else					return new ResponseEntity<>(list, HttpStatus.BAD_REQUEST);
+	}
+	
+	@GetMapping("/{hid}")
+	@ApiOperation(value = "취미 파티 상세조회")
+	public ResponseEntity<Chimi> search(@PathVariable Long hid){
+		Chimi newChimi = chimiService.findById(hid).get();
+		if(newChimi != null)	return new ResponseEntity<>(newChimi, HttpStatus.OK);
+		else					return new ResponseEntity<>(newChimi, HttpStatus.BAD_REQUEST);
 	}
 }
--- back/chimi-service/src/main/java/com/chimi/service/ChimiServiceImpl.java
+++ back/chimi-service/src/main/java/com/chimi/service/ChimiServiceImpl.java
@@ -1,8 +1,11 @@
 package com.chimi.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.chimi.model.Chimi;
@@ -21,5 +24,10 @@ public class ChimiServiceImpl implements ChimiService{
 	@Override
 	public Optional<Chimi> findById(Long hid) {
 		return chimiRepository.findById(hid);
+	}
+
+	@Override
+	public Page<Chimi> findAll(Pageable pageable) {
+		return chimiRepository.findAll(pageable);
 	}
 }
