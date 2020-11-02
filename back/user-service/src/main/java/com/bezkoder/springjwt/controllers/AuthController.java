54e41e61abd410cc1aafb4f30a21056125d1a56a
김형준 <khj58777@naver.com>
2020-11-02 21:42:54 +0900

    [bug] url변경

--- back/user-service/src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
+++ back/user-service/src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
@@ -85,7 +85,8 @@ public class AuthController {
 
 		
 		// Create new user's account
-		User user = new User(signUpRequest.getUsername(), 
+		User user = new User(
+				signUpRequest.getUsername(),
 							 signUpRequest.getEmail(),
 							 encoder.encode(signUpRequest.getPassword()),
 				signUpRequest.getNickname(),
