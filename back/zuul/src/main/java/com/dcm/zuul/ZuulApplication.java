4adb5d2a7c30eb50767624f615ab7debc85ad4d2
김지현 <rlawlgus322@naver.com>
2020-11-04 17:24:58 +0900

    [bug] S03P31A409-111 : cors 필터 추가

--- back/zuul/src/main/java/com/dcm/zuul/ZuulApplication.java
+++ back/zuul/src/main/java/com/dcm/zuul/ZuulApplication.java
@@ -3,12 +3,25 @@ package com.dcm.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulApplication {
 
+	@Bean
+	 public WebMvcConfigurer corsConfigurer () {
+	     return  new WebMvcConfigurer () {
+	         public  void addCorsMappings (CorsRegistry registry) {
+	            registry.addMapping ( "/ **" )
+	                    .allowedOrigins ( "*" )
+	                    .allowedMethods ( "GET" , "POST", "DELETE", "PUT" );
+	        }
+	    };
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
