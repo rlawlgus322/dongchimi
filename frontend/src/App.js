f6edfaa1528e44938ea289a84a3543dd6f7f331d
최윤주 <yeomju311@naver.com>
2020-11-04 13:36:23 +0900

    [feature] S03P31A409-108 : 자랑하기 게시판 목록

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEye, faHeart } from '@fortawesome/free-regular-svg-icons'
+import { faHeart as fasHeart, faCommentDots } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 import Main from './pages/Main';
 import Pages from './pages/Pages';
+
+library.add(faEye, faHeart, fasHeart, faCommentDots);
 
 function App() {
   return (
---
+++ frontend/src/components/Boast/Boast.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+// import './boast.css';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Sdiv = styled.div`
+margin-bottom: 30px;
+`;
+
+const Bdiv = styled.div`
+cursor: pointer;
+text-align: center;
+background-repeat: no-repeat;
+background-size: cover;
+background-position: center;
+height: 280px;
+position: relative;
+overflow: hidden;
+border-radius: 30px;
+`;
+
+const Mp = styled.p`
+margin: 0px;
+`;
+
+class Boast extends Component {
+
+  render() {
+    const backgroundImage = {
+      backgroundImage: `url(${this.props.boast.Boast.postImg})`
+    }
+    return (
+      <Sdiv className="col-md-4 col-lg-3 ">
+        <Mp>사진 {this.props.boast.username}</Mp>
+        <Bdiv style={backgroundImage}></Bdiv>
+        <FontAwesomeIcon icon={['far', 'eye']} />
+        <FontAwesomeIcon icon={['far', 'heart']} />
+        <FontAwesomeIcon icon={['fas', 'heart']} />
+        <FontAwesomeIcon icon={['fas', 'comment-dots']} />
+        <Mp>{this.props.boast.Boast.title}</Mp>
+      </Sdiv>
+    )
+  }
+}
+
+export default Boast;
+
