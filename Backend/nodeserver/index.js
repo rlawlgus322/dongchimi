6c591c543b0c60a17563ce2b46bf4b3bd314f351
김지현 <rlawlgus322@naver.com>
2020-11-09 22:00:37 +0900

    [~ing]  초기환경

--- Backend/nodeserver/index.js
+++ Backend/nodeserver/index.js
@@ -1,7 +1,33 @@
+//설치한 express 모듈 불러오기
 var express = require('express')
-var app = express()
+var socket = require('socket.io')
+const http = require('http')
+
+
+var app = express() //익스 프레스 객체 생성
+
+const server = http.createServer(app) //express http 서버 생성
+const io = socket(server) //생성된 서버를 socket.io에 바인딩
+
+server.listen(8080, function(){
+    console.log('서버 실행 중...')
+})
+
+io.sockets.on('connection',function(socket){
+    console.log('유저 접속 됨')
+
+    socket.on('send'.function(data){
+        console.log('전달된 메시지:',data.msg)
+    })
+
+    socket.on('disconnect',function(){
+        console.log('접속 종료')
+    })
+})
+
 app.listen(9999);
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-res.send('hello world')
+    res.send('hello world')
+
 })
