d40d768506c3bc1a5488b7f4a8600ce4cde8c0bc
최윤주 <yeomju311@naver.com>
2020-11-09 21:21:28 +0900

    [test] S03P31A409-134 : 소켓통신 테스트
    됐음 좋겠다

--- Backend/nodeserver/index.js
+++ Backend/nodeserver/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
-app.listen(9999);
+// app.listen(9999);
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-res.send('hello world')
+  res.send('hello world')
 })
