f1a6a45a93544bd7ec9af2053444eea2c4151194
최윤주 <yeomju311@naver.com>
2020-11-10 11:01:21 +0900

    [update] S03P31A409-36 : 탭 수정

--- frontend/src/components/mypage/MypageTab.js
+++ frontend/src/components/mypage/MypageTab.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Nav } from 'react-bootstrap';
-// import ReactDOM from 'react-dom';
+import Tab from '@material-ui/core/Tab';
+import TabContext from '@material-ui/lab/TabContext';
+import TabList from '@material-ui/lab/TabList';
+import TabPanel from '@material-ui/lab/TabPanel';
 import OpenParty from './OpenParty';
 import JoinParty from './JoinParty';
 import JoinedParty from './JoinedParty';
@@ -16,31 +18,28 @@ const obj = {
 class MypageTab extends Component {
   state = {
     activeTab: 0,
+    value: "1",
   }
-  clickHandler = (id) => {
-    this.setState({
-      activeTab: id
-    })
+  clickHandler = (e, id) => {
+    this.setState({ value: id });
   }
+
   render() {
     return (
-      <Nav className="wrapper">
-        <Nav variant="tabs">
-          <Nav.Item>
-            <Nav.Link onClick={() => this.clickHandler(0)}>내가 연 파티</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link onClick={() => this.clickHandler(1)}>참가한 파티</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link onClick={() => this.clickHandler(2)}>참가했던 파티</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link onClick={() => this.clickHandler(3)}>찜한 파티</Nav.Link>
-          </Nav.Item>
-        </Nav>
-        <div className="contents">{obj[this.state.activeTab]}</div>
-      </Nav>
+      <>
+        <TabContext value={this.state.value}>
+          <TabList onChange={this.clickHandler}>
+            <Tab label="내가 연 파티" value="1" />
+            <Tab label="참가한 파티" value="2" />
+            <Tab label="참가했던 파티" value="3" />
+            <Tab label="찜한 파티" value="4" />
+          </TabList>
+          <TabPanel value="1"><OpenParty /></TabPanel>
+          <TabPanel value="2"><JoinParty /></TabPanel>
+          <TabPanel value="3"><JoinedParty /></TabPanel>
+          <TabPanel value="4"><LikeParty /></TabPanel>
+        </TabContext>
+      </>
     );
   }
 }
