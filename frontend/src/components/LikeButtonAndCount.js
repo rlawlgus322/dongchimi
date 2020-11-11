323aa92e58c1a6fc79df4702b544ebc2e9fb198a
yeomju311@naver.com <yeomju311@naver.com>
2020-11-11 20:21:15 +0900

    [feature] Add LikeButtonAndCount component

---
+++ frontend/src/components/LikeButtonAndCount.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const LikeButtonAndCountBody = styled.span`
+  display: flex;
+  margin: 0px 5px;
+`;
+
+const LikeButton = styled.button`
+  border: none;
+  background-color: rgba(0, 0, 0, 0);
+  &::focus {
+    outline: none;
+  }
+`;
+
+const LikeCount = styled.span``;
+
+function LikeButtonAndCount(props) {
+  const { isLike, count } = props;
+  return (
+    <LikeButtonAndCountBody>
+      <LikeButton>
+        {isLike ? (
+          <FontAwesomeIcon icon={['fas', 'heart']} size="lg" color="crimson" />
+        ) : (
+          <FontAwesomeIcon icon={['far', 'heart']} size="lg" />
+        )}
+      </LikeButton>
+      <LikeCount>{count}</LikeCount>
+      <LikeCount />
+    </LikeButtonAndCountBody>
+  );
+}
+
+export default LikeButtonAndCount;
