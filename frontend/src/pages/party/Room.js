3bb154c6d9e149eef8e10f5bc142a1c90dc03d57
최윤주 <yeomju311@naver.com>
2020-11-11 13:13:41 +0900

    [bug] S03P31A409-141 : 될때됐는데..

--- frontend/src/pages/party/Room.js
+++ frontend/src/pages/party/Room.js
@@ -45,7 +45,7 @@ const Room = (props) => {
     const roomID = props.match.params.roomID;
 
     useEffect(() => {
-        socketRef.current = io.connect("/", { transforts: ['websocket'] });
+        socketRef.current = io.connect("/", { transforts: ['websocket'] }, { path: "/socket.io/" });
         navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then(stream => {
             userVideo.current.srcObject = stream;
             socketRef.current.emit("join room", roomID);
