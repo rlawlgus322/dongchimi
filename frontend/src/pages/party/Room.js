590599381d28134352d70af29094dac400d6a1cc
최윤주 <yeomju311@naver.com>
2020-11-11 10:06:44 +0900

    [bug] S03P31A409-141 : transports:websocket 명시
    될때됐다진짜

--- frontend/src/pages/party/Room.js
+++ frontend/src/pages/party/Room.js
@@ -45,7 +45,7 @@ const Room = (props) => {
     const roomID = props.match.params.roomID;
 
     useEffect(() => {
-        socketRef.current = io.connect("/");
+        socketRef.current = io.connect("/", { transforts: ['websocket'] });
         navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then(stream => {
             userVideo.current.srcObject = stream;
             socketRef.current.emit("join room", roomID);
