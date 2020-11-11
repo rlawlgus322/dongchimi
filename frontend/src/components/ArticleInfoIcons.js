d61569a6e5fd4461f8fe04c45dad19503884db4c
yeomju311@naver.com <yeomju311@naver.com>
2020-11-11 20:31:26 +0900

    [feature] Add ArticleInfoIcon component

--- frontend/src/components/ArticleInfoIcons.js
+++ frontend/src/components/ArticleInfoIcons.js
@@ -1,49 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LikeButtonAndCount from 'components/LikeButtonAndCount';
+import ViewConter from 'components/ViewCounter';
+import CommentConter from 'components/CommentCounter';
 
 const ArticleInfoIconsBody = styled.div`
   display: flex;
 `;
 
-const LikeButton = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0);
-  border: none;
-  &:focus {
-    outline: none;
-  }
-`;
-
-const Counter = styled.span`
-  margin-left: 5px;
-  margin-right: 10px;
-`;
-
 function ArticleInfoIcons(props) {
-  const [isLike, setIsLike] = useState(props.isLike);
-
-  const ClickLikeButton = () => {
-    setIsLike(!isLike);
-    // Todo: React Redux 쓰는거로 구조 싹 바꾸기
-  };
-
   return (
     <ArticleInfoIconsBody>
-      <FontAwesomeIcon icon={['far', 'eye']} size="lg" />
-      <Counter>10</Counter>
-      <LikeButton onClick={ClickLikeButton}>
-        {isLike ? (
-          <FontAwesomeIcon icon={['fas', 'heart']} size="lg" color="crimson" />
-        ) : (
-          <FontAwesomeIcon icon={['far', 'heart']} size="lg" />
-        )}
-      </LikeButton>
-      <Counter>10</Counter>
-      <FontAwesomeIcon icon={['fas', 'comment-dots']} size="lg" />
-      <Counter>10</Counter>
+      <ViewConter count={10} />
+      <LikeButtonAndCount isLike={true} count={50} />
+      <CommentConter count={15} />
     </ArticleInfoIconsBody>
   );
 }
