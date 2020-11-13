20d98f17f65b5ac912ce7868c1404f3155d2e590
최윤주 <yeomju311@naver.com>
2020-11-13 20:47:25 +0900

    [feature] S03P31A409-137 : 신청 수락 및 거절

--- frontend/src/components/Party/Read/PartyApplicant.js
+++ frontend/src/components/Party/Read/PartyApplicant.js
@@ -5,6 +5,7 @@ import api from '../../../utils/api';
 class PartyApplicant extends Component {
   state = {
     applicants: [],
+    userInfoList: [],
     chimiId: this.props.chimiId,
   }
 
@@ -12,13 +13,28 @@ class PartyApplicant extends Component {
     this.getApplicants();
   }
 
+  async componentDidUpdate() {
+    const users = [];
+    for (let i = 0; i < this.state.applicants.length; i++) {
+      await api.get(`/auth/userinfoby/${this.state.applicants[i].applicationPK.userId}`)
+        .then(({ data }) => {
+          // console.log('data', data);
+          users.push(data);
+        }).catch((err) => {
+          console.log(err);
+        })
+    }
+    if (users.length !== 0 && users.length !== this.state.userInfoList.length)
+      this.setState({ userInfoList: users });
+  }
+
   getApplicants() {
-    api.get('/hobby/apply/moderator', {
+    api.get(`/hobby/apply/moderator/${this.props.chimiId}`, {
       headers: {
         accessToken: sessionStorage.getItem('token'),
       }
     }).then(({ data }) => {
-      console.log('getApplicants', data);
+      // console.log('getApplicants', data);
       this.setState({ applicants: data });
     }).catch((err) => {
       console.log(err);
@@ -26,41 +42,54 @@ class PartyApplicant extends Component {
   }
 
   render() {
-    console.log('applicant chimiId', this.state.chimiId);
-    const applicants = this.state.applicants.map((applicant, index) => {
-      if (applicant.applicationPK.chimiId === Number(this.state.chimiId)) {
-        console.log('map ', applicant);
-        return (
-          <div key={index}>
-            지원한 사람 : {applicant.applicationPK.userId}
-            <button onClick={() => {
-              console.log('accept', applicant)
-              api.post('/hobby/enrolment', {
-                chimiId: applicant.applicationPK.chimiId,
-                userId: applicant.applicationPK.userId,
-              }, {
-                headers: {
-                  accessToken: sessionStorage.getItem('token'),
-                }
-              }).then((res) => {
-                console.log(res);
-                console.log('수락 성공');
-                this.getApplicants();
-              }).catch((err) => {
-                console.log(err);
-              })
-            }}>수락</button>
-            <button onClick={() => {
-              console.log('거절')
-            }}>거절</button>
-          </div>
-        )
-      }
+    // console.log('applicant chimiId', this.state.chimiId);
+    const applicants = this.state.userInfoList.map((userInfo, index) => {
+      // console.log('map ', userInfo);
+      return (
+        <div key={index}>
+          지원한 사람 : {userInfo.nickname}
+          별점 : {userInfo.star}
+          <button onClick={() => {
+            // console.log('accept', userInfo)
+            api.post('/hobby/enrolment', {
+              chimiId: this.state.applicants[index].applicationPK.chimiId,
+              userId: this.state.applicants[index].applicationPK.userId,
+            }, {
+              headers: {
+                accessToken: sessionStorage.getItem('token'),
+              }
+            }).then((res) => {
+              // console.log('수락 성공');
+              // console.log(res);
+              this.getApplicants();
+            }).catch((err) => {
+              console.log(err);
+            })
+          }}>수락</button>
+          <button onClick={() => {
+            api.delete('/hobby/apply/moderator', {
+              headers: {
+                accessToken: sessionStorage.getItem('token')
+              },
+              data: {
+                chimiId: this.state.applicants[index].applicationPK.chimiId,
+                userId: this.state.applicants[index].applicationPK.userId,
+              }
+            }).then((res) => {
+              // console.log('거절 성공')
+              // console.log(res);
+              this.getApplicants();
+            }).catch((err) => {
+              console.log(err);
+            })
+          }}>거절</button>
+        </div >
+      )
     })
 
     return (
       <>
-        {applicants[0] === undefined ? <h2>신청자가 없습니다</h2> : applicants}
+        { this.state.applicants.length === 0 ? <h2>신청자가 없습니다</h2> : applicants}
       </>
     )
   }
