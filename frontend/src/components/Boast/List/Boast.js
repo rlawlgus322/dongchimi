3c6047c102e001aef0a14a5cc87d0a16fe37ea2f
최윤주 <yeomju311@naver.com>
2020-11-14 17:06:10 +0900

    [feature] S03P31A409-166 : 자랑하기 목록 가져오기

--- frontend/src/components/Boast/List/Boast.js
+++ frontend/src/components/Boast/List/Boast.js
@@ -31,42 +31,39 @@ const ProfileImg = styled.img`
 
 class Boast extends Component {
   render() {
+    const path = JSON.parse(this.props.boast.boast.postImg)[0];
+    const image = "https://k3a409.p.ssafy.io" + path;
     const backgroundImage = {
-      backgroundImage: `url(${this.props.boast.Boast.postImg})`,
+      backgroundImage: `url(${image})`,
     };
     return (
       <Sdiv className="col-md-4 col-lg-3 ">
         <Mp>
           <ProfileImg src={this.props.boast.profileImage} />
-          {this.props.boast.username}
+          {this.props.boast.nickname}
         </Mp>
         <Bdiv
           style={backgroundImage}
           onClick={() =>
-            this.props.history.push(`/boast/${this.props.boast.Boast.bid}`)
+            this.props.history.push(`/boast/${this.props.boast.boast.bid}`)
           }
         ></Bdiv>
         <div className="row">
           <div className="col-4" style={{ padding: "0 20px" }}>
             <FontAwesomeIcon icon={['far', 'eye']} />
-            {this.props.boast.Boast.views}
+            {this.props.boast.boast.views}
           </div>
           <div className="col-4" style={{ padding: "0 20px" }}>
             {
-              this.props.boast.isLiked === false &&
-              <FontAwesomeIcon icon={['far', 'heart']} />
+              this.props.liked === true ? <FontAwesomeIcon icon={['fas', 'heart']} /> : <FontAwesomeIcon icon={['far', 'heart']} />
             }
-            {
-              this.props.boast.isLiked === true &&
-              <FontAwesomeIcon icon={['fas', 'heart']} />
-            }
-            {this.props.boast.Boast.likes}
+            {this.props.boast.boast.likes}
           </div>
           <div className="col-4" style={{ padding: "0 20px" }}>
             <FontAwesomeIcon icon={['fas', 'comment-dots']} />
           </div>
         </div>
-        <Mp>{this.props.boast.Boast.title}</Mp>
+        <Mp>{this.props.boast.boast.contents}</Mp>
       </Sdiv>
     );
   }
